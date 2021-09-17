b1884dc993b56716720eea68b990c2b6dfc0294f
Robby Findler <robby@racket-lang.org>
2021-09-17 15:20:19 -0500

    link the codes

--- codes.rkt
+++ codes.rkt
@@ -9,5 +9,8 @@
     (head (title "New Codes"))
     (body (table
            ,@(for/list ([faculty (in-list all-faculty)])
+               (define id (get-id faculty ids))
                `(tr (td ,faculty)
-                    (td (tt ,(get-id faculty ids)))))))))
+                    (td
+                     (a ((href ,(~a "/vote/" id)))
+                        (tt ,id)))))))))
