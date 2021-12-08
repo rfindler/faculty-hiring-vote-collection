abfeac86d0e6e86c2ecc53a448fa38508dbe5427
Robby Findler <robby@racket-lang.org>
2021-12-07 21:16:58 -0600

    make it illegal to give nothanks and any other proposal the same score

--- vote.rkt
+++ vote.rkt
@@ -1,6 +1,8 @@
 #lang racket
 (require "threaded-store.rkt" net/url
           web-server/http/request-structs)
+(module+ test (require rackunit))
+
 (provide
  (contract-out
   [votes-page
@@ -10,6 +12,7 @@
   (append (sort '("Systems" "Theory" "AI" "Interfaces" "Teaching-track") string<?)
           (list "Prefer not to say")))
 
+(define nothanks "nothanks")
 (define proposals+ids
   (list (list "Bio-inspired Robotics - Brenna presenting" "bio")
         (list "Collaborative AI - Ken presenting" "ai")
@@ -17,9 +20,15 @@
         (list "Parallel Systems - Peter presenting" "parallel")
         (list "Quantum - Nikos presenting" "quantum")
         (list "Vision - Mike presenting" "vision")
-        (list "Proposals scored below this do not have my support" "nothanks")))
+        (list "Proposals scored below this do not have my support" nothanks)))
 
-(define (valid-entry? n) (and (string? n) (real? (string->number n))))
+(define (valid-entry? n)
+  (cond
+    [(string? n)
+     (define num (string->exact-number n))
+     (and (real? num)
+          (exact? num))]
+    [else #f]))
 
 (define (build-url code)
   (url->string
@@ -33,6 +42,17 @@
              '()
              #f)))
 
+(define (string->exact-number ent)
+  (parameterize ([read-decimal-as-inexact #f])
+    (define n (string->number ent))
+    (and (exact? n) (real? n) n)))
+(module+ test
+  (check-equal? (string->exact-number "1") 1)
+  (check-equal? (string->exact-number "1/2") 1/2)
+  (check-equal? (string->exact-number "0.5") 1/2)
+  (check-equal? (string->exact-number "0.1") 1/10)
+  (check-true (exact? (string->exact-number "0.1"))))
+
 (define (votes-page code req)
   (define current (update-vote code req))
   (define normalized (make-hash))
@@ -41,11 +61,12 @@
                 (cond
                   [sum
                    (define ent (hash-ref current (list-ref p+id 1) #f))
-                   (define n (and (string? ent) (string->number ent)))
+                   (define n (and (string? ent) (string->exact-number ent)))
                    (and n (+ sum n))]
                   [else #f])))
   (define constituency-choice (hash-ref current constituency))
   (define dont-vote? (hash-ref current no-opinion))
+  (define nothanks-vote (hash-ref current nothanks #f))
   `(html
     (head (title "Vote for " ,code))
     (body
@@ -53,7 +74,8 @@
 
      (br)
      
-     (p "Indicate your preferences by putting a number (fraction and decimal notation are both okay) next to each proposal;"
+     (p "Indicate your preferences by putting a number (fraction and decimal"
+        " notation are both okay) next to each proposal;"
         " the larger the number the more you prefer it. The actual vote you"
         " cast will be normalized to sum to 1.")
 
@@ -72,12 +94,18 @@
                 (define this-proposal-score (hash-ref current id #f))
                 (define this-proposal-numeric-score
                   (and (string? this-proposal-score)
-                       (string->number this-proposal-score)))
+                       (string->exact-number this-proposal-score)))
 
                 (define invalid-reason
                   (cond
                     [(hash-ref current no-opinion #f) #f]
                     [(not (valid-entry? this-proposal-score)) '("This doesn't look like a number")]
+                    [(and (not (equal? id nothanks))
+                          (valid-entry? nothanks-vote)
+                          this-proposal-numeric-score
+                          (= (string->exact-number nothanks-vote)
+                             this-proposal-numeric-score))
+                     '("This has the same score as the cutoff")]
                     [else #f]))
                 `(tr (td ,@(if invalid-reason
                                (list `((style "color:red")))
@@ -89,8 +117,11 @@
                                  (onchange "this.form.submit()")
                                  (id ,id)
                                  (name ,id))))
-                     (td ,@(or invalid-reason
-                               (if (and sum this-proposal-numeric-score)
+                     (td ,@(if invalid-reason
+                               (list `((style "color:red")))
+                               (list))
+                         ,@(or invalid-reason
+                               (if (and sum this-proposal-numeric-score (not dont-vote?))
                                    (list "Normalized score: "
                                          (~r (/ this-proposal-numeric-score sum)))
                                    '())))))
